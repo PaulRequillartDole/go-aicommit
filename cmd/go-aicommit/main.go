1ec65d15c08f2e1470ae24b7ecfabeb700f14f93
PaulRequillartDole <requillart.paul@gmail.com>
2023-02-19 14:33:22 +0100

    Changement sur la gestion des erreurs

--- cmd/go-aicommit/main.go
+++ cmd/go-aicommit/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/PaulRequillartDole/go-aicommit/internal/aicommit"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,35 +11,28 @@ import (
 func main() {
 	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
 	output, err := cmd.Output()
-	if err != nil {
-		fmt.Println("Erreur lors de la vérification du dépôt Git :", err)
-		os.Exit(1)
-	}
-	if !strings.Contains(string(output), "true") {
+
+	if !strings.Contains(string(output), "true") || err != nil {
 		fmt.Println("Le répertoire actuel n'est pas un dépôt Git valide.")
 		os.Exit(1)
 	}
 
 	cmd = exec.Command("git", "diff", "--cached", "--", ".", "':(exclude)yarn.lock'", "':(exclude)package-lock.json'")
+	output, err = cmd.Output()
 
-	diff, err := cmd.Output()
-	if err != nil {
-		fmt.Println("Erreur lors de la récupération de git diff :", err)
-		os.Exit(1)
-	}
-
-	if len(diff) == 0 {
+	if len(output) == 0 || err != nil {
 		fmt.Println("Diff est vide, impossible de générer un message de commit.")
 		os.Exit(1)
 	}
 
-	commit := aicommit.New(1, diff)
+	commit := aicommit.New(1, output)
 
 	fmt.Println("Génération du message de commit 🤖")
 	message, err := commit.GenerateCommitMessage()
 
 	if err != nil {
-		log.Panic(err.Error())
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	fmt.Println(message)
